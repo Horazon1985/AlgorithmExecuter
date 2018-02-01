cbb9b816f903f180f6461f7e835c9704214a25eb
kovalens <kovalens@MSGN01630.int.root.msg.ag>
2018-02-01 10:59:49 +0100

    Nichtbenötigte Fehlerkonstanten beseitigt.

--- src/algorithmexecuter/exceptions/constants/AlgorithmExecutionExceptionIds.java
+++ src/algorithmexecuter/exceptions/constants/AlgorithmExecutionExceptionIds.java
@@ -5,7 +5,6 @@ public abstract class AlgorithmExecutionExceptionIds {
     public static final String AE_MAIN_NOT_FOUND = "AE_MAIN_NOT_FOUND";
     public static final String AE_ALGORITHM_NOT_ALL_INPUT_PARAMETERS_SET = "AE_ALGORITHM_NOT_ALL_INPUT_PARAMETERS_SET";
     public static final String AE_NO_SUCH_COMMAND = "AE_NO_SUCH_COMMAND";
-    public static final String AE_UNKNOWN_IDENTIFIER = "AE_UNKNOWN_IDENTIFIER";
     public static final String AE_RETURN_TYPE_EXPECTED = "AE_RETURN_TYPE_EXPECTED";
     public static final String AE_NULL_POINTER = "AE_NULL_POINTER";
     public static final String AE_MATRIXEXPRESSION_COULD_NOT_BE_SIMPLIFIED_TO_A_MATRIX = "AE_MATRIXEXPRESSION_COULD_NOT_BE_SIMPLIFIED_TO_A_MATRIX";
